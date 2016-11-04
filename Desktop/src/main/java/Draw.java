7fdfde669ce79151ea6c43a9a3e5ffe37a93be30
Zonglin Li <lizonglin6565@163.com>
2016-11-04 12:00:15 -0500

    draw line

--- Desktop/src/main/java/Draw.java
+++ Desktop/src/main/java/Draw.java
@@ -9,9 +9,9 @@ public class Draw {
     public static final int LINE = 0;
     public static final int HISTOGRAM = 1;
 
-    public void draw(ArrayList <Drawable> data, int mode) {
+    public void draw(ArrayList <Drawable> data, int mode) throws Exception{
+        int n = data.size();
         if (mode == HISTOGRAM) {
-            int n = data.size();
             for (int i = 0; i < n; i++) {
                 double x = 1.0 * i / n;
                 double y = data.get(i).getData();
@@ -19,6 +19,15 @@ public class Draw {
                 double rh = y;
                 StdDraw.filledRectangle(x, y, rw, rh);
             }
+        } else if (mode == LINE) {
+            StdDraw.setXscale(0, n);
+            StdDraw.setYscale(0, 1);
+            StdDraw.setPenRadius(0.01);
+            for (int i = 1; i <= n; i++) {
+                StdDraw.point(i, data.get(i).getData());
+            }
+        } else {
+            throw new Exception("Invalid drawing mode");
         }
     }
 }
